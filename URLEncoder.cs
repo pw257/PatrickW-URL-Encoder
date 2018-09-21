baa3c4143e10b618774755d1a19ff6504a719fb4
pw257 <42645110+pw257@users.noreply.github.com>
2018-09-21 16:49:08 -0500

    URL Encoder

---
+++ URLEncoder.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace URLEncoder
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string ProjectName;
+            string ActivityName;
+
+            Console.Write("Please enter your Project Name \n");
+            ProjectName = Console.ReadLine();
+
+            Console.Write("Please enter your Activity Name \n");
+            ActivityName = Console.ReadLine();
+            string url = "https://companyserver.com/content/[ProjectName]/files/[ActivityName]/[ActivityName]Report.pdf , ProjectName, ActivityName";
+
+        }
+    }
+}
